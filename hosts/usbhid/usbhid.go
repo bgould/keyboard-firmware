f570108c1820e1505b62d82a2ad0292d2aa0d189
BCG <bgould@users.noreply.github.com>
2022-01-30 00:38:21 -0500

    refactored HID interface

--- hosts/usbhid/usbhid.go
+++ hosts/usbhid/usbhid.go
@@ -12,12 +12,12 @@ import (
 const debug = false
 
 type Host struct {
-	kbd *usb.Keyboard
+	hid *usb.HID
 }
 
 func New(hid *usb.HID) *Host {
 	return &Host{
-		kbd: hid.Keyboard(),
+		hid: hid,
 	}
 }
 
@@ -25,11 +25,11 @@ func (host *Host) Send(rpt *keyboard.Report) {
 	if debug {
 		println(rpt[0], rpt[1], rpt[2], rpt[3], rpt[4], rpt[5], rpt[6], rpt[7])
 	}
-	host.kbd.SendKeys(rpt[0], rpt[2], rpt[3], rpt[4], rpt[5], rpt[6], rpt[7])
+	host.hid.SendKeys(rpt[0], rpt[2], rpt[3], rpt[4], rpt[5], rpt[6], rpt[7])
 }
 
 func (host *Host) LEDs() uint8 {
-	return host.kbd.LEDs()
+	return host.hid.KeyboardLEDs()
 }
 
 /*
