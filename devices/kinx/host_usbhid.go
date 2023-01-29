d7328e3df29408284ab4cfce8396bd3235bbc3a8
BCG <bcgould1@gmail.com>
2023-01-29 09:21:57 -0500

    Adding definitions for samd feathers

--- devices/kinx/host_usbhid.go
+++ devices/kinx/host_usbhid.go
@@ -1,4 +1,4 @@
-//go:build (nrf52840 || rp2040 || teensy41) && !host_tinyterm && !host_serial
+//go:build (feather_m0 || feather_m4 || nrf52840 || rp2040 || teensy41) && !host_tinyterm && !host_serial
 
 package main
 
