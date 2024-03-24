760c5369265e025c8bf05fb609ddaf71debf1b67
BCG <bgould@users.noreply.github.com>
2024-03-23 21:04:47 -0400

    Adding layer keycode support

--- devices/macropad-rp2040/main.go
+++ devices/macropad-rp2040/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/bgould/keyboard-firmware/hosts/usbvial"
 	"github.com/bgould/keyboard-firmware/hosts/usbvial/vial"
 	"github.com/bgould/keyboard-firmware/keyboard"
-	"github.com/bgould/keyboard-firmware/keyboard/keycodes"
 	"tinygo.org/x/drivers/encoders"
 	"tinygo.org/x/drivers/ws2812"
 	"tinygo.org/x/tinyfs/littlefs"
@@ -38,7 +37,11 @@ var (
 func init() {
 	initFilesystem()
 	configurePins()
+
+	usb.Manufacturer = "Adafruit"
+	usb.Product = "MacroPad RP2040"
 	usb.Serial = vial.MagicSerialNumber("")
+
 	encoder.Configure(encoders.QuadratureConfig{})
 	host.Configure()
 
@@ -56,45 +59,13 @@ func initFilesystem() {
 }
 
 func main() {
-
 	if backlight.Driver != nil {
 		backlight.Driver.SetBacklight(keyboard.BacklightOff, 0xFF)
 	}
-
 	board.ConfigureFilesystem()
 	board.EnableConsole(machine.Serial)
-
-	board.SetKeyAction(keyboard.KeyActionFunc(
-		func(key keycodes.Keycode, made bool) {
-			if host.UnlockStatus() != vial.UnlockInProgress {
-				switch key {
-				case keycodes.KC_FN12:
-					if made {
-						switch board.ActiveLayer() {
-						case 0:
-							board.SetActiveLayer(1)
-						case 1:
-							board.SetActiveLayer(0)
-						}
-						// println("layer:", board.ActiveLayer())
-						// fmt.Fprintf(serialer, "layer: %d\n", board.ActiveLayer())
-					}
-				default:
-					// fmt.Fprintf(serialer, "fn: %d %t\n", key-keycodes.FN0, made)
-				}
-			}
-		},
-	))
-
-	go func() {
-		for {
-			board.Task()
-			time.Sleep(500 * time.Microsecond)
-		}
-	}()
-
 	for {
-		time.Sleep(time.Hour)
+		board.Task()
+		time.Sleep(500 * time.Microsecond)
 	}
-
 }
